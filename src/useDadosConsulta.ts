64831c1f59586ba55d0c3f8513cb0b5715071de7
andersonpdsoff <apdsengsoft@gmail.com>
2025-04-24 10:07:29 -0400

    foi modificar o endpoint para poder vir de um  json npoint online

---
+++ src/useDadosConsulta.ts
@@ -0,0 +1,9 @@
+
+import IConsulta from "./types/IConsulta"
+import useFetch from "./useFetch"
+
+const useDadosConsulta = () => {
+    return useFetch<IConsulta[]>('consultas');
+}
+
+export default useDadosConsulta
--- src/useFetch.ts
+++ src/useFetch.ts
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 
-
-export default function useFetch<T>() {
+export default function useFetch<T>(url: string) {
     const [dados, setDados] = useState<T | null>(null);
     const [erro, setErro] = useState('');
 
     useEffect(() => {
-        fetch(`http://localhost:8080/${url}`).then(
-            resposta => resposta.json()
-        ).then(dados => setDados(dados)).catch((erro => setErro(erro)))
-    }, [url])
+        fetch('https://api.npoint.io/084ddaa5a39b05fc6b1b')
+            .then(resposta => resposta.json())
+            .then(dados => setDados(dados))
+            .catch(erro => setErro(erro.message));
+    }, [url]);
 
     return { dados, erro };
-
 }
